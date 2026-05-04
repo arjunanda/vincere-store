7680fbe7f963b398430730e7185ee4e4e6098358
Muhammad Arjunanda <dawatidawati31@gmail.com>
2026-05-04 23:09:22 +0700

    fix news

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         Paginator::defaultView('vendor.pagination.dashboard');
 
         // Share Website Settings & Payment Methods globally (Cached for performance)
